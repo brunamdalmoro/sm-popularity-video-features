026714974b55e718a1bf0e5fcd171b3ff6ac0a0c
Bruna Martini Dalmoro <brunamdalmoro@hotmail.com>
2019-12-01 15:27:12 -0300

    fixes data flow in data processing

--- scripts/data processing/split_visual_and_temporal_features.R
+++ scripts/data processing/split_visual_and_temporal_features.R
@@ -1,10 +1,10 @@
 # Split data in two: visual features and temporal features
 library(tidyverse)
 
-facebook_data <- readRDS("data/raw/facebook_data.rds")
+facebook_dataset <- readRDS("data/raw/facebook_dataset.rds")
 
-visual_features <- select(facebook_data, facebook_post_id:title.sentiment)
-temporal_features <- select(facebook_data, facebook_post_id, views.1:likes.168)
+visual_features <- select(facebook_dataset, facebook_post_id:text_detection.average_text_frames)
+temporal_features <- select(facebook_dataset, facebook_post_id, views.1:likes.168)
 
 write_rds(x = visual_features, path = "data/processed/visual_features.rds")
 write_rds(x = temporal_features, path = "data/processed/temporal_features.rds")
